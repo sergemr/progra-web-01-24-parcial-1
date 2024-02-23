50fceffc495802869c7e4b9ee80c901915840c33
Mirito10 <mathias.calderon@ulatina.net>
2024-02-22 20:07:45 -0600

    Update App.js

--- examen_MathiasCalderon/examen_mathiascalderon/src/App.js
+++ examen_MathiasCalderon/examen_mathiascalderon/src/App.js
@@ -39,7 +39,7 @@ function App() {
                   </div>
 
               </form>
-              </div>
+              </div>                                                                                                                                                    
           </header>
       </div>
   );
